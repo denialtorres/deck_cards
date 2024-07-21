ecc4d1a28688e09bf9957a9be280ecb68a3ea66f
denialtorres <denial.torres@gmail.com>
2024-07-21 12:13:23 -0600

    Implemet deck of cards with immutable bindings

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,29 @@ struct Deck {
 }
 
 fn main() {
-    let deck = Deck { cards: vec![] };
+    // List of suits - 'hearts', 'spades
+    // List of values - 'ace', 'two', 'three'
+
+    // Double nested for loop
+    //
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = [
+        "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+        "Queen", "King",
+    ];
+
+    let mut cards = vec![];
+    // double nested for loop
+    //
+    for suit in suits {
+        for value in values {
+            let card = format!("{value} of {suit}");
+
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
 
     println!("Here is your deck: {deck:?}");
 }
