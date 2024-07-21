b1a2aca98a7a2a4eb8a7794e50f15b705fe7aa27
denialtorres <denial.torres@gmail.com>
2024-07-21 13:06:11 -0600

    shuffle cards using crates packages

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
+use rand::{seq::SliceRandom, thread_rng};
+
 #[derive(Debug)]
 
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // List of suits - 'hearts', 'spades
-    // List of values - 'ace', 'two', 'three'
+impl Deck {
+    fn new() -> Self {
+        // List of suits - 'hearts', 'spades
+        // List of values - 'ace', 'two', 'three'
 
-    // Double nested for loop
-    //
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    let values = [
-        "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
-        "Queen", "King",
-    ];
+        // Double nested for loop
+        //
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = [
+            "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+            "Queen", "King",
+        ];
 
-    let mut cards = vec![];
-    // double nested for loop
-    //
-    for suit in suits {
-        for value in values {
-            let card = format!("{value} of {suit}");
+        let mut cards = vec![];
+        // double nested for loop
+        //
+        for suit in suits {
+            for value in values {
+                let card = format!("{value} of {suit}");
 
-            cards.push(card);
+                cards.push(card);
+            }
         }
+
+        // implicit return
+        Deck { cards }
     }
 
-    let deck = Deck { cards };
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
 
-    println!("Here is your deck: {deck:?}");
+        self.cards.shuffle(&mut rng);
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+
+    deck.shuffle();
+
+    println!("Here is your deck: {deck:#?}");
 }
